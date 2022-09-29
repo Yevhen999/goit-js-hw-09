2ed8dd0c6d49831428b183ff5186a9088b2af65d
Yevhen <leshchenkoyevhen@gmail.com>
2022-09-29 23:33:59 +0300

    completed task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+const formRef = document.querySelector('.form');
+const delayRef = document.querySelector('[name="delay"]');
+const stepRef = document.querySelector('[name="step"]');
+const amountRef = document.querySelector('[name="amount"]');
+
+formRef.addEventListener('submit', event => {
+  event.preventDefault();
+  let amount = 0;
+  let timeDelay = Number(delayRef.value);
+  let step = Number(stepRef.value);
+  let time = timeDelay - step;
+  for (let i = 0; i < Number(amountRef.value); i += 1) {
+    amount += 1;
+    time += step;
+    createPromise(amount, time)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+
+        // Reject
+      }
+    }, delay);
+  });
 }
